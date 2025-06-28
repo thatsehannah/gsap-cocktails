284a578aa2d9260c76597043b4045123799ac063
E. Hannah <echannah631@gmail.com>
2025-06-28 15:40:02 -0400

    setting up initial styles for project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl text-indigo-300'>Hello World</h1>
+    </div>
+  );
 };
 
 export default Home;
