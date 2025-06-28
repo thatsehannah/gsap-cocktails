763fedd7d78f11a8a120da357ac97069d34cadb5
E. Hannah <echannah631@gmail.com>
2025-06-28 17:37:01 -0400

    animating the hero and paragraph text in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,41 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+      delay: 1,
+    });
+  }, []);
+
   return (
     <>
       <section
         id='hero'
-        className='noisy'
+        className='noisy '
       >
         <h1 className='title'>MOJITO</h1>
         <Image
@@ -30,6 +59,14 @@ const Hero = () => {
               <p className='subtitle'>
                 Sip the spirit <br /> of Summer
               </p>
+            </div>
+            <div className='view-cocktails'>
+              <p className='subtitle'>
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <Link href='#cocktails'>View Cocktails</Link>
             </div>
           </div>
         </div>
