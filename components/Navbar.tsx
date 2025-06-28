262b0ae3e8081a675d3609d2ab9be2fc53bcafcc
E. Hannah <echannah631@gmail.com>
2025-06-28 17:03:03 -0400

    adding tween to animate the navbar to have a transparent background when scrolling down on page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,32 @@
 import { navLinks } from "@/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top", //when the bottom of the navbar reaches the top of the viewport, that's when the animation is applied
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
   return (
     <nav>
       <div>
