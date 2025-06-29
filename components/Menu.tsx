16a92f8f2dd3a41613c783c05aa2916c59880685
E. Hannah <echannah631@gmail.com>
2025-06-29 00:43:38 -0400

    animating the text and images in menu section whenever the slider is cycling through the different cocktails

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { allCocktails } from "@/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 import React, { useRef, useState } from "react";
 
 const Menu = () => {
   const contentRef = useRef<HTMLDivElement>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useGSAP(() => {
+    gsap.fromTo("#title", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+    gsap.fromTo(
+      "#cocktail-img",
+      { opacity: 0, xPercent: -100 },
+      { xPercent: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+    );
+
+    gsap.fromTo(
+      ".details h2",
+      { yPercent: 100, opacity: 0 },
+      { yPercent: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+    );
+    gsap.fromTo(
+      ".details p",
+      { yPercent: 100, opacity: 0 },
+      { yPercent: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+    );
+  }, [currentIndex]);
 
   const totalCocktails = allCocktails.length;
 
@@ -111,6 +134,7 @@ const Menu = () => {
         <div className='cocktail'>
           <Image
             src={currentCocktail.image}
+            id='cocktail-img'
             className='object-contain'
             alt={currentCocktail.name}
             height={540}
